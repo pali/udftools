b943f48f61ca68f8e0c9b708f2c15171d4f9e356
Ben Fennema <bfennema@falcon.csc.calpoly.edu>
2000-03-25 05:08:21 +0000

    Applied Jens Axboe's patch

--- src/pktsetup.c
+++ src/pktsetup.c
@@ -1,6 +1,24 @@
 /*
  * Quite possibly the ugliest piece I've ever written...
+ *
+ * Copyright (c) 1999,2000	Jens Axboe <axboe@suse.de>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  */
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
@@ -62,8 +80,8 @@ void setup_dev(char *pkt_device, char *device, int rem)
 
 void usage(void)
 {
-	printf("pktsetup /dev/pktcddvd0 /dev/hdd\tsetup device\n");
-	printf("pktsetup -d /dev/pktcddvd\ttear down device\n");
+	printf("pktsetup /dev/pktcdvd0 /dev/hdd\tsetup device\n");
+	printf("pktsetup -d /dev/pktcdvd0\ttear down device\n");
 }
 
 int main(int argc, char **argv)
