9e2633e5084f63e3fdcd39512b35f604d6fd6139
Vojtech Vladyka <vojtech.vladyka@gmail.com>
2016-10-21 13:09:57 +0200

    Just storing progress...

--- udffsck/main.c
+++ udffsck/main.c
@@ -20,12 +20,52 @@
  *
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <ecma_167.h>
+
+#include "udf.h"
 #include "options.h"
 
 int main(int argc, char *argv[])
 {
     parse_args(argc, argv);	
-    
-    
+
+    int counter;
+    FILE *fp;
+    long pos;
+    //  struct rec my_record;
+    char identifier[6];
+    unsigned char type;
+    unsigned char version;
+    char data[2041];
+
+    if ((fp = fopen("/home/rain/Development/udf/udf-samples/image.iso", "rb")) == NULL) {
+        fprintf(stderr, "%s %i:", __FILE__, __LINE__);
+        perror(NULL);
+        return errno;
+    } 
+
+    fseek(fp,  0x8000, SEEK_SET);
+    pos = ftell(fp);
+    printf("Pos: 0x%lx\n", pos);
+    fread(&type, 1,1,fp);
+    fread(&identifier,5,1,fp);
+    fread(&version, 1,1,fp);
+    fread(&data, 2041,1,fp);
+    printf("Type: 0x%x\nIdentifier: %s\nVersion: 0x%x\nData: %s\n", type, identifier, version, data+1);
+    //    for ( counter=1; counter <= 10; counter++)
+    //    {
+    //        my_record.x= counter;
+    //        fwrite(&my_record, sizeof(struct rec), 1, ptr_myfile);
+    //    }
+    fclose(fp);
+
+
+    return 0; 
+
     return 0;
 }
---
+++ udffsck/udf.h
@@ -0,0 +1,8 @@
+#ifndef __UDF_H__
+#define __UDF_H__
+
+#define BLOCK_SIZE 2048
+#define LOC_CDFS_DESCRIPTOR 0x10
+
+
+#endif /*__UDF_H__*/
