c9da13b426b0a43c5ae28e2582d18450825e4ce2
Vojtech Vladyka <vojtech.vladyka@gmail.com>
2017-07-09 12:25:44 +0200

    Splitted tests to separate script

---
+++ udffsck/travis-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ..
+wget --no-check-certificate https://pac.chapadla.cz/~rain/udffsck-test-samples/udf-samples.tar.xz 
+bash decompress-samples.sh
+cd udftools/udffsck
+./test
+cd ../..
+rm udf-samples.tar.xz udf-samples -r
+wget --no-check-certificate https://pac.chapadla.cz/~rain/udffsck-test-samples/udf-samples-extra.tar.xz 
+bash decompress-samples-extra.sh
+cd udftools/udffsck
+./testextra
