0e976ccc69a0c268171e982f9a05afad89ae8055
Vojtech Vladyka <vojtech.vladyka@gmail.com>
2017-12-28 08:28:01 +0100

    Updated FOSS headers

--- udffsck/log.c
+++ udffsck/log.c
@@ -1,8 +1,5 @@
 /*
- * log.c
- *
- * Copyright (c) 2017    Vojtech Vladyka <vojtch.vladyka@gmail.com>
- * All rights reserved.
+ * Copyright (C) 2017 Vojtech Vladyka <vojtech.vladyka@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,12 +11,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 
- * USA.
- *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 #include "config.h"
 
 #include <stdarg.h>
